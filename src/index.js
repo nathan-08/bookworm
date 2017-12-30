59b979498161cc2a1cd5babfcd847d5f6a152806
nathan-08 <nathanryan001@gmail.com>
2017-12-30 10:21:05 -0700

    semantic ui setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import "semantic-ui-css/semantic.min.css";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <HashRouter>
