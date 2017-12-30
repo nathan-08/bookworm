da5dca372a8dcceede8c2fe1448fbaa9c100a965
nathan-08 <nathanryan001@gmail.com>
2017-12-30 10:14:03 -0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from "./components/pages/HomePage"
+import LoginPage from "./components/pages/LoginPage"
 
-
-const App = () => <div> Hello World </div>;
+const App = () => <div>
+  <Route path="/" exact component={HomePage} />
+  <Route path="/login" component={LoginPage} />
+</div>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+        <App />
+    </HashRouter>, document.getElementById('root'));
 registerServiceWorker();
